d41dbad57f8f3c1a93f7108e280a5a1018d78fab
Felix | ileFix <fefe3011.fd@gmail.com>
2026-07-02 15:32:01 +0200

    better reload script

--- reload.sh
+++ reload.sh
@@ -1,4 +1,6 @@
+git pull
 cargo build --release
 sudo systemctl stop inertia-relayer
 sudo cp target/release/inertia-relayer /etc/inertia-relayer/
 sudo systemctl start inertia-relayer
+echo "tail /etc/inertia-relayer/relayer.log -f -n 100"
